1eb57c31f30276164c1917b900fccca78bb85e84
Moksh Jain <mokshjain.10a.m.com>
2024-10-15 10:25:47 +0530

    Completed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,33 +1,44 @@
 import React from "react";
 import { FaInstagram, FaFacebookF, FaTwitter } from "react-icons/fa";
+import { Fade } from "react-reveal";
 
 const Form = () => {
   return (
-    <div className="m-2 w-full md:w-[55%] flex justify-center text-white">
+    <div className="m-2 w-full md:w-[55%] flex justify-center text-white z-0">
       <div className="space-y-4 xl:space-y-5 justify-center flex-col text-center w-[90%] md:w-[80%] mt-[10%] md:mt-[6%]">
-        <h1 className="text-5xl xl:text-6xl font-medium">HAVE ANY QUERY ?</h1>
+        <Fade top>
+          <h1 className="text-5xl xl:text-6xl font-medium">HAVE ANY QUERY?</h1>
+        </Fade>
 
         {/* email form */}
         <div className="space-y-5 justify-center ">
-          <input
-            type="text"
-            placeholder="Full Name"
-            className="border-black border-2 rounded w-full p-2"
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            className="border-black border-2 rounded w-full p-2"
-          />
-          <textarea
-            placeholder="Message"
-            className="border-black border-2 rounded w-full p-2"
-            rows="5"
-          />
+          <Fade top>
+            <input
+              type="text"
+              placeholder="Full Name"
+              className=" hover:border-violet-500 cursor-pointer text-black border-2 rounded w-full p-2"
+            />
+          </Fade>
+          <Fade top>
+            <input
+              type="email"
+              placeholder="Email"
+              className="hover:border-violet-500 cursor-pointer text-black border-2 rounded w-full p-2"
+            />
+          </Fade>
+          <Fade top>
+            <textarea
+              placeholder="Message"
+              className="hover:border-violet-500 cursor-pointer text-black border-2 rounded w-full p-2"
+              rows="5"
+            />
+          </Fade>
 
-          <button className="border-white border-2 rounded px-[6%] py-[0.5%] text-xl xl:text-2xl font-semibold">
-            SEND
-          </button>
+          <Fade top>
+            <button className="border-white border-2 rounded px-[6%] py-[0.5%] text-xl xl:text-2xl font-semibold hover:bg-violet-900 hover:border-violet-900  cursor-pointer hover:scale-[5px] transition-transform duration-500">
+              SEND
+            </button>
+          </Fade>
         </div>
 
         <hr className="border-white" />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { FaTimes } from "react-icons/fa";
+import { Fade } from "react-reveal";
 
 const Header = () => {
   const [open, setOpen] = useState(false);
@@ -18,13 +19,23 @@ const Header = () => {
       {/* code for md and lg screen */}
       <div className="w-full hidden md:block">
         <ul className="justify-end flex md:space-x-6 lg:space-x-10 text-xl xl:text-3xl  xl:mt-4 font-medium mr-[7%]">
-          <li>Home</li>
-          <li>Our Team</li>
-          <li>Events</li>
-          <li>Gallery</li>
-          <li className="rounded-full px-[3%] py-[0.2%] text-center cursor-pointer bg-violet-500 text-white">
-            Contact us
-          </li>
+          <Fade right>
+            <li className="cursor-pointer">Home</li>
+          </Fade>
+          <Fade right>
+            <li className="cursor-pointer">Our Team</li>
+          </Fade>
+          <Fade right>
+            <li className="cursor-pointer">Events</li>
+          </Fade>
+          <Fade right>
+            <li className="cursor-pointer">Gallery</li>
+          </Fade>
+          <Fade right>
+            <li className="rounded-full px-[3%] py-[0.2%] text-center cursor-pointer bg-violet-500 text-white">
+              Contact us
+            </li>
+          </Fade>
         </ul>
       </div>
 
@@ -35,15 +46,28 @@ const Header = () => {
             <button onClick={hamburger} className="absolute right-5 top-3">
               <FaTimes size={30} color="white" />
             </button>
-            <ul className="absolute right-3 top-10 justify-end space-y-4 text-xl font-medium mr-[7%] bg-black w-[50%] text-center pb-[10%] px-[5%] ">
-              <li>Home</li>
-              <li>Our Team</li>
-              <li>Events</li>
-              <li>Gallery</li>
-              <li className="rounded-full px-[2%] py-[0.2%] text-center cursor-pointer bg-violet-500 text-white">
-                Contact
-              </li>
-            </ul>
+            <Fade right>
+              {" "}
+              <ul className="absolute z-10 right-3 top-10 justify-end space-y-4 text-xl font-medium mr-[7%] bg-black w-[50%] text-center pb-[10%] px-[5%] ">
+                <Fade right>
+                  <li className="cursor-pointer">Home</li>
+                </Fade>
+                <Fade right>
+                  <li className="cursor-pointer">Our Team</li>
+                </Fade>
+                <Fade right>
+                  <li className="cursor-pointer">Events</li>
+                </Fade>
+                <Fade right>
+                  <li className="cursor-pointer">Gallery</li>
+                </Fade>
+                <Fade right>
+                  <li className="rounded-full px-[2%] py-[0.2%] text-center cursor-pointer bg-violet-500 text-white">
+                    Contact
+                  </li>
+                </Fade>
+              </ul>
+            </Fade>
           </div>
         ) : (
           <div className="w-full flex md:hidden">
